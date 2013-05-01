e01cf593cfde47be72da5d9993bb8c1b36890373
Jason Kulatunga <jk17@ualberta.ca>
2013-05-01 10:31:51 -0700

    added package.json file for easy installation/update via npm. boostrap package not yet installed.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require( 'express' ),
  routes = require('./routes'),
+ path = require('path')
  engine = require('ejs-locals')
  app = express(),
  lessMiddleware = require('less-middleware');
@@ -13,10 +14,17 @@ app.configure( function (){
   app.set( 'views', __dirname + '/views' );
   app.set( 'view engine', 'ejs' );
   app.use( express.favicon());
+  
+  //bootstrap-less configuration.
+  var bootstrapPath = path.join(__dirname, 'node_modules', 'bootstrap');
+  app.use('/img', express.static(path.join(bootstrapPath, 'img')));
   app.use(lessMiddleware({
         src: __dirname + '/public',
+		paths  : [path.join(bootstrapPath, 'less')],
         compress: true
   }));
+  
+  
   app.use( express.static( __dirname + '/public' ));
   app.use( express.logger());
   app.use( express.bodyParser());
